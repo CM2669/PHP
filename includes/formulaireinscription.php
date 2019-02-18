af5f3fe46e00efda74b304b68d2cf92c31053fee
christophe moutier <17666@csmrouen.net>
2019-02-18 10:37:09 +0100

    ajout prénom

--- includes/formulaireinscription.php
+++ includes/formulaireinscription.php
@@ -9,6 +9,12 @@ if (!isset($mail))$mail = "";
         <label for="nom">Nom&nbsp;:</label>
         <input type="text" id="nom" name="nom" value="<?=$nom?>"/>
         <div>
+
+            <label for="prenom">Prenom&nbsp;:</label>
+            <input type="text" id="prenom" name="prénom" value="<?=$prenom?>"/>
+
+            <div>
+
             <label for="mail">Mail&nbsp;:</label>
             <input type="text" id="mail" name="mail "value="<?=$mail?>"/>
 
@@ -24,6 +30,8 @@ if (!isset($mail))$mail = "";
 
                 </div>
                 <input type="hidden" name="Maurice">
+
+            </div>
 
 
             </div>
